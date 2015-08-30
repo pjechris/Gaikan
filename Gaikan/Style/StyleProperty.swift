41705a4cb42fafa432d890d352c5edd00d6c2eac
pjechris <jcpastant@xebia.fr>
2015-08-30 11:31:44 +0200

    [style] defined basic classes

---
+++ Gaikan/Style/StyleProperty.swift
@@ -0,0 +1,15 @@
+//
+// This file is part of Gaikan
+//
+// Created by JC on 30/08/15.
+// For the full copyright and license information, please view the LICENSE
+// file that was distributed with this source code
+//
+
+import Foundation
+
+public enum StyleProperty {
+    case Border
+    case Color
+    case Font
+}

541999caeb871d7432a92990acfb1910e4bce523
pjechris <jcpastant@xebia.fr>
2015-09-03 10:06:57 +0200

    [project] added sample

---
+++ Sample/ViewController.swift
@@ -0,0 +1,30 @@
+//
+// This file is part of Gaikan
+//
+// Created by JC on 03/09/15.
+// For the full copyright and license information, please view the LICENSE
+// file that was distributed with this source code
+//
+
+import UIKit
+import Gaikan
+
+class ViewController: UIViewController {
+    @IBOutlet weak var label : UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let style: StyleRule = [ .Color: UIColor.redColor(), .TintColor: UIColor.blueColor() ]
+
+        self.label.applyStyle(style)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

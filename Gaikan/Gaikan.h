909109b76fb693e55e7eb8ccf817008219eeae02
pjechris <jcpastant@xebia.fr>
2015-09-05 19:46:19 +0200

    [misc]

--- Gaikan/Gaikan.h
+++ Gaikan/Gaikan.h
@@ -1,9 +1,9 @@
 //
-//  Gaikan.h
-//  Gaikan
+// This file is part of Gaikan
 //
-//  Created by JC on 30/08/15.
-//  Copyright © 2015 fr.akane. All rights reserved.
+// Created by JC on 30/08/15.
+// For the full copyright and license information, please view the LICENSE
+// file that was distributed with this source code
 //
 
 #import <UIKit/UIKit.h>

ff1b11ab4d29fef608fde0bc295e79fa20b7b1a0
pjechris <jcpastant@xebia.fr>
2015-10-14 18:29:41 +0200

    [style] added pseudo class enum

---
+++ Gaikan/Style/StylePseudoClass.swift
@@ -0,0 +1,20 @@
+//
+// This file is part of Gaikan
+//
+// Created by JC on 14/10/15.
+// For the full copyright and license information, please view the LICENSE
+// file that was distributed with this source code
+//
+
+import Foundation
+
+public enum StylePseudoClass {
+    case Normal
+
+    case Disabled
+    case Enabled
+    case Focus
+    case Highlighted
+    case On
+    case Selected
+}
